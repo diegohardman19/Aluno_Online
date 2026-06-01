e4f82103f188db074b9ca9a5ddda269502922f92
diego hardman <diegohardman50@gmail.com>
2026-06-01 16:42:13 -0300

    Update DisciplinaController.java

--- api/src/main/java/br/com/alunoonline/api/controller/DisciplinaController.java
+++ api/src/main/java/br/com/alunoonline/api/controller/DisciplinaController.java
@@ -5,6 +5,7 @@ import br.com.alunoonline.api.service.DisciplinaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/disciplinas")
@@ -15,8 +16,14 @@ public class DisciplinaController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void criarDisciplina(@RequestBody Disciplina disciplina){
+    public void criarDisciplina(@RequestBody Disciplina disciplina) {
         disciplinaService.criarDisciplina(disciplina);
+    }
 
+    @GetMapping("/professor/{professorId}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Disciplina> listarDisciplinasDoProf(
+            @PathVariable Long professorId) {
+        return disciplinaService.listarDisciplinarDoProf(professorId);
     }
 }
