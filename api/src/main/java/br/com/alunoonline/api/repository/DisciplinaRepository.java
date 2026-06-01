f8c4bd270bf9bf17d1201a638acf2f85d63221bb
diego hardman <diegohardman50@gmail.com>
2026-06-01 16:47:17 -0300

    Update DisciplinaRepository.java

--- api/src/main/java/br/com/alunoonline/api/repository/DisciplinaRepository.java
+++ api/src/main/java/br/com/alunoonline/api/repository/DisciplinaRepository.java
@@ -2,8 +2,7 @@ package br.com.alunoonline.api.repository;
 
 import br.com.alunoonline.api.model.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long > {
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+    List<Disciplina> findByProfessorId(Long professorId);
 }
