c4647b641b5f5adba1a6b1798aaca91405d5604c
diego hardman <diegohardman50@gmail.com>
2026-06-01 17:11:04 -0300

    Update ProfessorRepository.java

--- api/src/main/java/br/com/alunoonline/api/repository/ProfessorRepository.java
+++ api/src/main/java/br/com/alunoonline/api/repository/ProfessorRepository.java
@@ -1,4 +1,5 @@
 package br.com.alunoonline.api.repository;
+
 import br.com.alunoonline.api.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
